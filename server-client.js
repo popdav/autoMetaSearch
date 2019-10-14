c3ebebfa0b7b2b10ffe1c43adfb31a2fb23ded5b
akinovak <akinovak@gmail.com>
2019-10-14 13:06:21 +0200

    merging algorithm

--- server-client.js
+++ server-client.js
@@ -20,6 +20,51 @@ const polovniModel = require('./models/PolovniAutomobili')
 const mojModel = require('./models/MojAuto')
 
 
+app.post('/merge', (req, res) => {
+
+    let arr1 = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    let arr2 = [2, 2, 2, 2, 2, 2, 2, 2]
+
+    let mergedArr = []
+
+    let first_out = 0
+    let second_out = 0
+
+    let order = 0
+
+    while(first_out < arr1.length || second_out < arr2.length) {
+
+        let rand = Math.floor(Math.random() * 3) + 1;
+
+        if(order === 0) {
+            if(first_out + rand < arr1.length) {
+                mergedArr = mergedArr.concat(arr1.slice(first_out, first_out + rand))
+                first_out = first_out + rand
+            }
+            else {
+                mergedArr = mergedArr.concat(arr1.slice(first_out, arr1.length))
+                mergedArr = mergedArr.concat(arr2.slice(second_out, arr2.length))
+                break
+            }
+        }
+        else {
+            if(second_out + rand < arr2.length) {
+                mergedArr = mergedArr.concat(arr2.slice(second_out, second_out + rand))
+                second_out = second_out + rand
+            }
+            else {
+                mergedArr = mergedArr.concat(arr2.slice(second_out, arr2.length))
+                mergedArr = mergedArr.concat(arr1.slice(first_out, arr1.length))
+                break
+            }
+        }
+
+        order = (order + 1) % 2
+    }
+
+    res.send(mergedArr)
+});
+
 app.post('/findPolovni', (req, res) => {
     let body = req.body.findQuery
     console.log(req.body)
